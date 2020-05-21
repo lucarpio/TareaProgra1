1d8c2d64fc0ad2183dc832ef29966b920f881d3d
lucarpio <201610755a@gmail.com>
2020-05-20 22:11:42 -0500

    subiendo version 1 de tarea

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tarea_progra.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Formulario.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Tarea_progra.Models
+{
+    public class Formulario{
+        public int sueldo{ get; set; }
+
+        public int resultado{ get; set; }
+        public int meses{ get; set; }
+
+        public string mensaje{ get; set; }
+
+    }
+}
